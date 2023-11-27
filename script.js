de3aee7c2e9453771088376737f83a84eaec7ea4
Iwegbuvictor <102805790+Iwegbuvc@users.noreply.github.com>
2023-11-27 14:53:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// =========== Typing animation =========
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Pedagogue",
+    "Web Developer",
+    "Programmer",
+    "Chemical Engineer",
+  ],
+  typeSpeed: 500,
+  backSpeed: 50,
+  loop: true,
+});
